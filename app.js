8296a33fcafc732ac589a28b48d9028338e3f848
Praveen Jaiprakash <92042769+missionalpha2502@users.noreply.github.com>
2023-01-10 15:30:45 +0530

    Day1 of JavaScript
    This is Day 1 of learning JavaScript.

---
+++ app.js
@@ -0,0 +1,18 @@
+//OBJECTIVE:
+//In this challenge, we review some basic concepts that will get you started with this series.
+//You will need to use the same (or similar) syntax to read input and write output in challenges throughout HackerRank.
+
+//TASK:
+//To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line,
+//and finally print the value of your variable on a second line.
+
+
+//SOLUTION:- 
+
+function processData(inputString) {
+    // This line of code prints the first line of output
+    console.log("Hello, World.");
+
+    // Write the second line of output that prints the contents of 'inputString' here.
+    console.log(inputString);
+}
